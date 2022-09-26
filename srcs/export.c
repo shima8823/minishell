7c8f72576c6cbd1b9365e3841c62b86d95e89ed3
らいか <raika4789@gmail.com>
2022-09-26 18:25:29 +0900

    exportの原型完成

--- srcs/export.c
+++ srcs/export.c
@@ -6,7 +6,70 @@
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 17:35:04 by takanoraika       #+#    #+#             */
-/*   Updated: 2022/09/26 17:35:24 by takanoraika      ###   ########.fr       */
+/*   Updated: 2022/09/26 18:24:21 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+t_shell	g_shell;
+
+static void	export_vars()
+{
+	int	i;
+	
+	i = 0;
+	while (g_shell.vars[i] != NULL)
+	{
+		printf("declare -x %s", g_shell.vars[i]);
+		i++;
+	}
+}
+
+static void	vars_cpy_to_strs(char **strs)
+{
+	
+}
+
+static char	*arrange_var(char *arg)
+{
+	
+}
+
+static int	add_vars(char *arg)
+{
+	char	**tmp;
+
+	tmp = ft_calloc(g_shell.vars_len, sizeof(char **));
+	if (tmp == NULL)
+	{
+		perror("minishell: export: malloc is failed");
+		exit(EXIT_FAILURE);
+	}
+	vars_cpy_to_strs(tmp);
+	tmp[g_shell.vars_len - 1] = arrange_var;
+	tmp[g_shell.vars_len] = NULL;
+	g_shell.vars_len ++;
+	return (0);
+}
+
+void	command_export(char **args)
+{
+	int	i;
+	int	err_flg;
+	int	err_argc;
+
+	err_flg = 0;
+	if (args == NULL)
+		export_vars();
+	while (args[i] != NULL)
+	{
+		if (add_vars(args[i]) == 1)
+		{
+			err_flg = 1;
+			err_argc = i;
+		}
+		i ++;
+	}
+	if (err_flg != 0)
+		error_in_export(args[err_argc]);
+}
--- srcs/export2.c
+++ srcs/export2.c
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   export2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/14 11:20:30 by shima             #+#    #+#             */
-/*   Updated: 2022/09/26 18:25:00 by takanoraika      ###   ########.fr       */
+/*   Created: 2022/09/26 18:23:48 by takanoraika       #+#    #+#             */
+/*   Updated: 2022/09/26 18:24:34 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../includes/minishell.h"
 
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdbool.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include "../libft/libft.h"
-
-typedef struct s_shell {
-	char	**vars;
-	int		vars_len;
-}				t_shell;
-
-void	signal_set(void);
-void	error_in_export(char *arg);
-
-#endif
+void	error_in_export(char *arg)
+{
+	perror("minishell: export: `=': not a valid identifier");
+	exit(EXIT_FAILURE);
+}
