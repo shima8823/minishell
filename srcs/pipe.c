10a6bbd3a89fc05236e9a1029ab2256adeae4946
らいか <raika4789@gmail.com>
2022-10-07 10:43:09 +0900

    微修正

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -6,7 +6,7 @@
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 20:29:55 by takanoraika       #+#    #+#             */
-/*   Updated: 2022/10/02 10:52:34 by takanoraika      ###   ########.fr       */
+/*   Updated: 2022/10/03 12:27:37 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 extern char **environ;
 
-int ft_pipe(t_pipe *p_info, int fdd)
+int ft_pipe(t_pipe *p_info, int bf_fd)
 {
 	int		fd[2];
 	pid_t	pid;
@@ -28,18 +28,19 @@ int ft_pipe(t_pipe *p_info, int fdd)
 		close(fd[1]);
 		return (-1);
 	}
-	else if (pid == 0)
+	if (pid == 0)
 	{
 		close(fd[0]);
-		dup2(fdd, 0);
-		close(fdd);
+		dup2(bf_fd, 0);
+		close(bf_fd);
 		if (p_info->next != NULL)
 		{
 			dup2(fd[1], 1);
 			close(fd[1]);
 		}
 		if (execve(p_info->cmd,p_info->arg,environ) == -1)
-			return (-1);
+			return (1);
+		return (0);
 	}
 	return (fd[0]);
 }
@@ -59,12 +60,15 @@ int main(void)
 	p_info_1.next = &p_info_2;
 	p_info_2.cmd = "/usr/bin/grep";
 	argv2[0] = "grep";
-	argv2[1] = "mini";
+	argv2[1] = "a";
 	argv2[2] = NULL;
 	p_info_2.arg = argv2;
+	// p_info_2.next = &p_info_1
 	p_info_2.next = NULL;
 	fdd = ft_pipe(&p_info_1, 0);
-	ft_pipe(&p_info_2, fdd);
+	fdd = ft_pipe(&p_info_2, fdd);
+	argv2[1] = "o";
+	// fdd = ft_pipe(&p_info_2, fdd);
 	return 0;
 }
 
