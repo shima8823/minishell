1dafca67e1f8831bd3e9f4b0a0cb57a64908e705
らいか <raika4789@gmail.com>
2022-10-12 12:32:07 +0900

    refactoring

--- srcs/execution/pipe.c
+++ srcs/execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 20:29:55 by takanoraika       #+#    #+#             */
-/*   Updated: 2022/10/12 12:13:06 by takanoraika      ###   ########.fr       */
+/*   Updated: 2022/10/12 12:32:05 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,33 +48,32 @@ static void	link_pipe(int fd[2], int read_fd, t_pipe *p_info)
 	close(fd[PIPE_WRITE]);
 }
 
-int main(void)
-{
-	t_pipe p_info_1;
-	t_pipe p_info_2;
-	char *argv1[2];
-	char *argv2[3];
-	int fdd;
+// int main(void)
+// {
+// 	t_pipe p_info_1;
+// 	t_pipe p_info_2;
+// 	char *argv1[2];
+// 	char *argv2[3];
+// 	int fdd;
 
-	p_info_1.cmd = "/bin/ls";
-	argv1[0] = "ls";
-	argv1[1] = NULL;
-	p_info_1.arg = argv1;
-	p_info_1.next = &p_info_2;
-	p_info_2.cmd = "/usr/bin/grep";
-	argv2[0] = "grep";
-	argv2[1] = "a";
-	argv2[2] = NULL;
-	p_info_2.arg = argv2;
-	p_info_2.next = &p_info_1;
-	// p_info_2.next = NULL;
-	fdd = ft_pipe(&p_info_1, 0);
-	fdd = ft_pipe(&p_info_2, fdd);
-	argv2[1] = "o";
-	fdd = ft_pipe(&p_info_2, fdd);
-	argv2[1] = "f";
-	p_info_2.next = NULL;
-	fdd = ft_pipe(&p_info_2, fdd);
-	return 0;
-}
-
+// 	p_info_1.cmd = "/bin/ls";
+// 	argv1[0] = "ls";
+// 	argv1[1] = NULL;
+// 	p_info_1.arg = argv1;
+// 	p_info_1.next = &p_info_2;
+// 	p_info_2.cmd = "/usr/bin/grep";
+// 	argv2[0] = "grep";
+// 	argv2[1] = "a";
+// 	argv2[2] = NULL;
+// 	p_info_2.arg = argv2;
+// 	p_info_2.next = &p_info_1;
+// 	// p_info_2.next = NULL;
+// 	fdd = ft_pipe(&p_info_1, 0);
+// 	fdd = ft_pipe(&p_info_2, fdd);
+// 	argv2[1] = "o";
+// 	fdd = ft_pipe(&p_info_2, fdd);
+// 	argv2[1] = "f";
+// 	p_info_2.next = NULL;
+// 	fdd = ft_pipe(&p_info_2, fdd);
+// 	return 0;
+// }
