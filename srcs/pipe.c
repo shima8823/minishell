f4ef7465d4b5cd38fe8d36a07c22df3c322195f0
らいか <raika4789@gmail.com>
2022-10-12 11:22:24 +0900

    複数のpipeに対応

--- srcs/pipe.c
+++ srcs/pipe.c
@@ -6,12 +6,13 @@
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 20:29:55 by takanoraika       #+#    #+#             */
-/*   Updated: 2022/10/09 12:47:14 by takanoraika      ###   ########.fr       */
+/*   Updated: 2022/10/12 11:21:34 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+void	link_pipe(int fd[2], int read_fd, t_pipe *p_info);
 extern char **environ;
 #define PIPE_READ 0
 #define PIPE_WRITE 1
@@ -32,23 +33,23 @@ int ft_pipe(t_pipe *p_info, int read_fd)
 	}
 	if (pid == 0)
 	{
-		close(fd[PIPE_READ]);
-		dup2(read_fd, STDIN_FILENO);
-		close(read_fd);
-		printf("test1\n");
-		if (p_info->next != NULL)
-		{
-			dup2(fd[PIPE_WRITE], STDOUT_FILENO);
-			close(fd[PIPE_WRITE]);
-		}
-		printf("test2\n");
-		if (execve(p_info->cmd,p_info->arg,environ) == -1)
-			return (-1);
+		link_pipe(fd, read_fd, p_info);
+		execve(p_info->cmd,p_info->arg, environ);
 	}
-	printf("pid == %d\n", pid);
-	// waitpid(pid, NULL, 0);
-	printf("pipe finish\n");
-	return (fd[0]);
+	close(fd[PIPE_WRITE]);
+	return (fd[PIPE_READ]);
+}
+
+void	link_pipe(int fd[2], int read_fd, t_pipe *p_info)
+{
+	close(fd[PIPE_READ]);
+	dup2(read_fd, STDIN_FILENO);
+	close(read_fd);
+	if (p_info->next != NULL)
+	{
+		dup2(fd[PIPE_WRITE], STDOUT_FILENO);
+		close(fd[PIPE_WRITE]);
+	}
 }
 
 int main(void)
@@ -69,13 +70,13 @@ int main(void)
 	argv2[1] = "a";
 	argv2[2] = NULL;
 	p_info_2.arg = argv2;
-	// p_info_2.next = &p_info_1;
-	p_info_2.next = NULL;
+	p_info_2.next = &p_info_1;
+	// p_info_2.next = NULL;
 	fdd = ft_pipe(&p_info_1, 0);
-	printf("first pipe finish\n");
 	fdd = ft_pipe(&p_info_2, fdd);
 	argv2[1] = "o";
-	// fdd = ft_pipe(&p_info_2, fdd);
+	p_info_2.next = NULL;
+	fdd = ft_pipe(&p_info_2, fdd);
 	return 0;
 }
 
