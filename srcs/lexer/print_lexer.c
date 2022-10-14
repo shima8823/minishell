36b83dfd7eb1aef7b4c8dac0856da54776ceaf1d
shima8823 <taiyaki1275@gmail.com>
2022-10-14 20:38:27 +0900

    update: refecter

--- srcs/lexer/print_lexer.c
+++ srcs/lexer/print_lexer.c
@@ -6,37 +6,35 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 11:05:46 by shima             #+#    #+#             */
-/*   Updated: 2022/10/05 17:58:26 by shima            ###   ########.fr       */
+/*   Updated: 2022/10/13 14:53:06 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include "../../includes/lexer.h"
 
-void print_lexer(t_lexer *lexer_buf)
+void print_tokens(t_token *token)
 {
 	char	*type;
-	t_token	*lst;
 
 	type = NULL;
 	printf("===========lexer===========\n");
-	lst = lexer_buf->list_tokens;
-	while (lst)
+	while (token)
 	{
-		if (lst->type == CHAR_PIPE)
+		if (token->type == CHAR_PIPE)
 			type = "PIPE";
-		else if (lst->type == CHAR_GREATER)
+		else if (token->type == CHAR_GREATER)
 			type = "GREATER";
-		else if (lst->type == CHAR_LESSER)
+		else if (token->type == CHAR_LESSER)
 			type = "LESSER";
-		else if (lst->type == D_GREATER)
+		else if (token->type == D_GREATER)
 			type = "D_GREATER";
-		else if (lst->type == D_LESSER)
+		else if (token->type == D_LESSER)
 			type = "D_LESSER";
-		else if (lst->type == TOKEN)
+		else if (token->type == TOKEN)
 			type = "TOKEN";
-		printf("{[%s], %s}\n", lst->data, type);
-		lst = lst->next;
+		printf("{[%s], %s}\n", token->data, type);
+		token = token->next;
 	}
 	printf("===========================\n");
 }
