19a59acabfd8cd72c2cdcac31b895f5f2eaa5cfd
shima8823 <taiyaki1275@gmail.com>
2022-10-14 20:38:20 +0900

    update: refecter

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,29 +6,22 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 11:50:14 by shima             #+#    #+#             */
-/*   Updated: 2022/10/09 22:15:25 by shima            ###   ########.fr       */
+/*   Updated: 2022/10/13 15:16:33 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include "../includes/lexer.h"
 
-static void	init_buf(t_lexer **buf, size_t line_size);
-static void	tokenizer(t_lexer *lexer_buf, char *line, size_t line_size);
-
-t_token	*token_new(size_t data_size, t_token_type type);
-
-bool	lexer(t_lexer **lexer_buf, char *line)
+bool	lexer(t_token **token, char *line)
 {
-	bool	ret;
-	size_t	line_size;
+	bool	is_missing_quote;
+	bool				ret;
 
-	line_size = ft_strlen(line);
-	init_buf(lexer_buf, line_size);
-	tokenizer(*lexer_buf, line, line_size);
+	*token = tokenizer(line, &is_missing_quote);
 	if (DEBUG)
-		print_lexer(*lexer_buf);
-	if ((*lexer_buf)->is_missing_quote)
+		print_tokens(*token);
+	if (is_missing_quote)
 	{
 		if (ft_putendl_fd(QUOTE_ERROR_MSG, STDERR_FILENO) == -1)
 			error_exit("ft_putendl_fd");
@@ -37,14 +30,16 @@ bool	lexer(t_lexer **lexer_buf, char *line)
 	return (true);
 }
 
-static void	tokenizer(t_lexer *lexer_buf, char *line, size_t line_size)
+t_token	*tokenizer(char *line, bool *is_missing_quote)
 {
 	t_tokenizer_info	info;
+	t_token				*start;
 	t_token				*lst;
 
 	ft_bzero(&info, sizeof(t_tokenizer_info));
-	lst = lexer_buf->list_tokens;
-	info.line_size = line_size;
+	info.line_size = ft_strlen(line);
+	start = token_new(info.line_size, 0);
+	lst = start;
 	while (line[info.line_i])
 	{
 		if (info.state == STATE_GENERAL)
@@ -54,17 +49,9 @@ static void	tokenizer(t_lexer *lexer_buf, char *line, size_t line_size)
 		(info.line_i)++;
 	}
 	lst->data[info.data_i] = '\0';
-	if (info.state != STATE_GENERAL)
-		lexer_buf->is_missing_quote = true;
-}
-
-static void	init_buf(t_lexer **buf, size_t line_size)
-{
-	*buf = malloc(sizeof(t_lexer));
-	if (!(*buf))
-		error_exit("malloc");
-	ft_bzero(*buf, sizeof(t_lexer));
-	(*buf)->list_tokens = token_new(line_size, 0);
+	if (is_missing_quote && info.state != STATE_GENERAL)
+		*is_missing_quote = true;
+	return (start);
 }
 
 t_token	*token_new(size_t data_size, t_token_type type)
@@ -80,4 +67,17 @@ t_token	*token_new(size_t data_size, t_token_type type)
 	new->type = type;
 	new->next = NULL;
 	return (new);
+}
+
+void	free_tokens(t_token *token)
+{
+	t_token	*tmp;
+
+	while (token)
+	{
+		tmp = token->next;
+		free(token->data);
+		free(token);
+		token = tmp;
+	}
 }
