9f50082b315afbc3757c44587e7934bd57126aee
shima8823 <taiyaki1275@gmail.com>
2022-10-14 20:45:24 +0900

    add: connect expansion

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,17 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 11:20:13 by shima             #+#    #+#             */
-/*   Updated: 2022/10/10 11:13:08 by shima            ###   ########.fr       */
+/*   Updated: 2022/10/14 20:44:59 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include "../includes/expansion.h"
 
 void		prompt(void);
 int			parse_command(char **args);
 bool		is_command(char *input, char *command);
 char		**split_line(char *line);
-
-// lexer
-void	print_lexer(t_lexer *lexer_buf);
-void	free_lexer(t_lexer *lexer_buf);
 
 int main(int argc, char *argv[])
 {
@@ -42,7 +39,7 @@ void	prompt(void)
 	char	*line;
 	char	**args;
 	int		status;
-	t_lexer	*lexer_buf;
+	t_token	*token;
 	t_ast	*node;
 	
 	while (true)
@@ -52,11 +49,18 @@ void	prompt(void)
 		if (!line)
 			error_exit("readline");
 		args = split_line(line);
-		if (lexer(&lexer_buf, line))
-			parser(&node, lexer_buf->list_tokens);
+		if (!lexer(&token, line))
+		{
+			free_tokens(token);
+			continue ;
+		}
+		parser(&node, token);
+		expansion(&node);
+		if (DEBUG)
+			print_ast(node);
 		status = execution(node);
 		// printf("%s\n", line);
-		free_lexer(lexer_buf);
+		free_tokens(token);
 		free_ast(node);
 		free_array(args);
 		free(line);
@@ -76,22 +80,6 @@ char	**split_line(char *line)
 		exit(EXIT_FAILURE);
 	}
 	return (args);
-}
-
-void	free_lexer(t_lexer *lexer_buf)
-{
-	t_token	*lst;
-	t_token	*tmp;
-
-	lst = lexer_buf->list_tokens;
-	while (lst)
-	{
-		tmp = lst->next;
-		free(lst->data);
-		free(lst);
-		lst = tmp;
-	}
-	free(lexer_buf);
 }
 
 void	error_exit(const char *s)
