f680d1728a0df9f4d6ec08079c9430efc2e23895
らいか <raika4789@gmail.com>
2022-10-23 23:34:06 +0900

    norm redirect_here.c

--- srcs/execution/redirect_here.c
+++ srcs/execution/redirect_here.c
@@ -6,22 +6,36 @@
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 12:17:55 by takanoraika       #+#    #+#             */
-/*   Updated: 2022/10/23 19:45:55 by takanoraika      ###   ########.fr       */
+/*   Updated: 2022/10/23 23:33:11 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static void	here_doc(char **res, int fd[2], t_command cmd);
+
 int	input_redirect_here(t_command cmd)
 {
-	char	*line;
 	char	*res;
-	char	*tmp;
 	int		fd[2];
 
 	set_signal_here();
 	wpipe(fd);
 	res = ft_wcalloc(1, 1);
+	here_doc(&res, fd, cmd);
+	ft_putstr_fd(res, fd[PIPE_WRITE]);
+	wdup2(fd[PIPE_READ], STDIN_FILENO);
+	wclose(fd[PIPE_READ]);
+	wclose(fd[PIPE_WRITE]);
+	free(res);
+	return (0);
+}
+
+static void	here_doc(char **res, int fd[2], t_command cmd)
+{
+	char	*tmp;
+	char	*line;
+
 	while (1)
 	{
 		line = readline(">");
@@ -35,22 +49,12 @@ int	input_redirect_here(t_command cmd)
 		}
 		tmp = line;
 		line = ft_strjoin(line, "\n");
-		if (!line)
-			exit(EXIT_FAILURE);
 		if (tmp)
 			free(tmp);
-		tmp = res;
-		res = ft_strjoin(res, line);
-		if (!res)
-			exit(EXIT_FAILURE);
+		tmp = *res;
+		*res = ft_strjoin(*res, line);
 		if (tmp)
 			free(tmp);
 		free(line);
 	}
-	ft_putstr_fd(res, fd[PIPE_WRITE]);
-	wdup2(fd[PIPE_READ], STDIN_FILENO);
-	wclose(fd[PIPE_READ]);
-	wclose(fd[PIPE_WRITE]);
-	free(res);
-	return (0);
 }
